472dfecbcb1713131087486a52b69f4f72524617
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-23 22:17:42 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,5 @@
-
+model_path = "models/colorization_release_v2.caffemodel"
+pro_text_path = "models/models_colorization_deploy_v2.prototxt"
+points_path = "models/pts_in_hull.npy"
+input_path = "input"
+output_path = "output"
